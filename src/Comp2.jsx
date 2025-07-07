e38c906e94499a42261c65aee4fdc44c79b0d520
Miroslav <999Striker999@gmail.com>
2025-07-07 22:49:15 +0200

    changing components

--- src/Comp2.jsx
+++ src/Comp2.jsx
@@ -1,5 +1,4 @@
 const Comp2 = ({numb, str, bool, obj, func, arr}) =>{
-    console.log(numb, str, bool, obj, func,  arr)
     return <h1>
         {numb} {str} {bool? 'взлетает' : 'не взлетает'} {obj.name} {func()} {arr}
     </h1>
