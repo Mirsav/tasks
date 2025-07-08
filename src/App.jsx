2cd52c98b6a3634a72b11a6d3456448c0f722b0b
Miroslav <999Striker999@gmail.com>
2025-07-09 00:20:00 +0200

    adding component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import But from './But.jsx'
 
-function App() {
-  
+const App = () => {
+  const [numb, setState] = useState(1)
+  const [showHide, setShowHide] = useState(true)
+  const [text, setText] = useState('')
+  const [color, setColor] = useState('blue')
+  const style={color: color}
+
   return (
-    <>
-      
-    </>
+  <>
+  <But />
+  state: {numb}
+  <button onClick={() => setState(numb+1)}>click</button>
+  <button onClick={() => setShowHide(!showHide)}>{showHide ? 'Скрыть' : 'Показать'}</button>
+      {showHide && <p>Опа!</p>}
+  <p><input type='text' value={text} onChange={e=>setText(e.target.value)}/></p>
+  {<p>Вы ввели: {text}</p>}
+  
+  <p><button onClick={()=> setColor(color === 'blue' ? 'green' : 'blue')}>Change Color</button></p>
+  <p style={style}>Поменяй мне цвет</p>
+  </>
   )
 }
 
---
+++ src/But.jsx
@@ -0,0 +1,5 @@
+const But =()=>{
+    return <h3> Приступим к заданию! </h3>
+}
+
+export default But
