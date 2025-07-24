62ab4832a8113eb068a7a6e10a5016467259eae6
Miroslav <999Striker999@gmail.com>
2025-07-24 22:10:53 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComp from './MyComp'
 
-function App() {
-  
+
+function App (){
+  const [state, setState] = useState(1)
   return (
     <>
+  {state %2 == 0 ? <MyComp number={state}/> : <h1>Пусто!!!</h1>}
+  <button onClick={()=> setState(state=>state+1)}>KILL</button>
     </>
   )
 }
