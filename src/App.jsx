4ac94a7579ab1c81e4a2c2e09e4ee8a56061e897
Miroslav <999Striker999@gmail.com>
2025-08-13 23:38:18 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React, { userEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import List from "./List";
 
 function App() {
+
   return (
     <>
-    <List/>
+    <List arr={['First ','Second ','Third ','Fourth ', 'Fifth ']}/>
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,38 +1,24 @@
 import { useState, useRef, useEffect } from "react";
 
-function List() {
-  const [text, setText] = useState("");
-  const [student, setStudent] = useState([
-    { id: 1, name: "Igor", age: 27 },
-    { id: 2, name: "Vitya", age: 15 },
-    { id: 3, name: "Sasha", age: 35 },
-    { id: 4, name: "Lena", age: 22 },
-  ]);
+function List({ arr }) {
+    const [items, setItem] = useState(arr)
 
-  const inputRef = useRef(null);
-
-  useEffect(()=>{
-    inputRef.current.focus()
-  },[])
+    const addItem = (index)=>{
+        const newItem = [...items]
+        newItem[index] = `!!!${newItem[index]}`
+        setItem(newItem)
+    }
 
   return (
-    <>
-      <input
-      ref = {inputRef} 
-      onChange={(e) => setText(e.target.value)}
-      value={text} 
-      />
-      {student.map((item) => {
-        return (
-          <div key={item.id}>
-            <p>{item.name}</p>
-            <p>{item.age}</p>
-            -----------------
-          </div>
-        );
-      })}
-    </>
-  );
+  <>
+  {items.map((item, index) => (
+    <li key={index}>
+        {item}
+        <button onClick={()=>addItem(index)}>Add</button>
+    </li>
+  ))}
+  </>
+  )
 }
 
 export default List;
