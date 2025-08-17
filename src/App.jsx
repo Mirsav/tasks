401583ee5252e7a0a8ad1d09980dad055daef065
Miroslav <999Striker999@gmail.com>
2025-08-17 20:44:11 +0200

    create new branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import List from "./List";
 
 function App() {
-  const [items, setItems] = useState([
-    "First",
-    "Second",
-    "Third",
-    "Fourth",
-    "Fifth",
-  ]);
-  const [value, setValue] = useState("");
-  const res = useRef(null);
-
-  const addItem = (index) => {
-    const newItem = [...items];
-    newItem[index] = `!!!${newItem[index]}`;
-    setItems(newItem);
-  };
-
-  function focusInput() {
-    res.current.focus();
-  }
-  const handleInput = (e) => {
-    if (e.key === "Enter" && value.trim() !== "") {
-      setItems([...items, value]);
-      setValue("");
-    }
-  };
 
   return (
     <>
-      <input
-        ref={res}
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onKeyDown={handleInput}
-      />
-      <button onClick={focusInput}>Фикс</button>
-      <List items={items} onUpdateItem={addItem} />
     </>
   );
 }
