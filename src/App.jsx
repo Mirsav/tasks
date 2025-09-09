552f30ea60362ae0ae3a24d2403dbe675a3798c6
Miroslav <999Striker999@gmail.com>
2025-09-09 21:47:45 +0200

    remake

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
+import Obj from "./Obj";
+import Arr from "./Arr";
+import ArrObj from "./ArrObj";
 
 function App() {
-
   return (
     <>
+      <Obj />
+      <Arr />
+      <ArrObj />
     </>
-  )
+  );
 }
 
 export default App;
