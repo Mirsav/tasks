700745553b5357f6e512fa74b874f40c7af673ee
Miroslav <999Striker999@gmail.com>
2025-09-10 22:05:01 +0200

    new task

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./components/GlobalStyle";
-import { lightTheme, darkTheme } from "./components/Themes";
-import { useDarkMode } from "./components/useDarkMode";
-import Toggle from "./components/Toggler";
 import "./App.css";
-import Obj from "./Obj";
-import Arr from "./Arr";
-import ArrObj from "./ArrObj";
 
 function App() {
-  const [theme, themeToggler, mountedComponent] = useDarkMode();
-  const themeMode = theme === 'light' ? lightTheme : darkTheme;
-
-  if (!mountedComponent) return <div />;
 
   return (
-    <ThemeProvider theme={themeMode}>
       <>
-        <GlobalStyles />
-        <Toggle theme={theme} toggleTheme={themeToggler} />
-        <Obj />
-        <Arr />
-        <ArrObj />
       </>
-    </ThemeProvider>
   );
 }
 
