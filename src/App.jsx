b0fc1983676aae556f58bd5c37d6fef30ee0d9fa
Miroslav <999Striker999@gmail.com>
2025-09-12 00:11:37 +0200

    context is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { ControlsPanel } from "./ControlsPanel";
+import { LangSwitch } from "./LanguageContext";
+import { ThemeSwitch } from "./ThemeContext";
+import { Header } from "./Header";
+import { UserProfile } from "./UserProfile";
 
 function App() {
-
   return (
-      <>
-      </>
+    <>
+      <LangSwitch>
+        <ThemeSwitch>
+          <Header />
+          <ControlsPanel />
+          <UserProfile />
+        </ThemeSwitch>
+      </LangSwitch>
+    </>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useLanguage } from "./LanguageContext";
+import { useTheme } from "./ThemeContext";
+import { translations } from "./translations";
+
+export const Header = () => {
+  const { language } = useLanguage();
+  const { theme } = useTheme();
+
+  const themeStyles = {
+    light: {
+      backgroundColor: "#f0f0f0",
+      color: "#333",
+    },
+    dark: {
+      backgroundColor: "#333",
+      color: "#fff",
+    },
+  };
+
+  return (
+    <header
+      style={{
+        padding: "20px",
+        margin: "10px",
+        ...themeStyles[theme],
+      }}
+    >
+      <h1>{translations[language].welcome}</h1>
+    </header>
+  );
+};
