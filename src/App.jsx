df529ca7cb19ae08f08e2b0c46065af9c361d97d
Miroslav <999Striker999@gmail.com>
2025-11-03 23:15:02 +0100

    empty branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import "./App.css";
-import { ControlsPanel } from "./ControlsPanel";
-import { LangSwitch } from "./LanguageContext";
-import { ThemeSwitch } from "./ThemeContext";
-import { Header } from "./Header";
-import { UserProfile } from "./UserProfile";
 
 function App() {
   return (
     <>
-      <LangSwitch>
-        <ThemeSwitch>
-          <Header />
-          <ControlsPanel />
-          <UserProfile />
-        </ThemeSwitch>
-      </LangSwitch>
     </>
   );
 }
